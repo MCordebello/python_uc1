d65b5c6e992705160773b727775d5ba92f1519e2
MCordebello <moniquecordebello@outlook.com>
2025-03-07 20:42:29 -0300

    Atividades aula 03

---
+++ aula03_tarefa02.py
@@ -0,0 +1,37 @@
+a=input("Digite o 1º numero : ")
+a=float(a)
+b=input("Digite o 2º numero : ")
+b=float(b)
+soma=a+b
+sub=a-b
+a*b
+multiplicação=a*b
+divisão=a/b
+
+##
+#print(soma)
+#print("O resultado da soma é")
+
+##
+#Metodo pt_BR
+##
+print(f"O resultado da soma é {soma}")
+
+##
+#print("O resultado da subtração é")
+
+##
+#Metodo pt_BR
+##
+print(f"O resultado da subtração é {sub}")
+
+##
+#print
+
+##
+#print(multiplicação)
+#print("O resultado da multiplicação é")
+
+##Metodo pt_BR
+##
+print(f"O resultado da multiplicação é {multiplicação}")
